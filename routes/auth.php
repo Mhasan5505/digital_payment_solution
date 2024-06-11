a5070d6e3e5656fa15b7ae90b3801f85479bb247
Mhasan5505 <Mh12256634@gmail.com>
2024-06-11 13:31:09 +0600

    first commit

---
+++ routes/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\PasswordResetController;
+use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Auth\VerifyEmailController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('login',LoginController::class);
+Route::post('logout',LogoutController::class);
+Route::post('register',RegisterController::class);
+Route::post('password/email', [PasswordResetController::class, 'sentLinkEmail'])->name('pass.reset.link');
+Route::post('password/reset', [PasswordResetController::class, 'reset'])->middleware('signed')->name('password.reset');
+Route::post('email/verify/send', [VerifyEmailController::class, 'sendMail']);
+Route::post('email/verify', [VerifyEmailController::class, 'verify'])->middleware('signed')->name('verify-email');
+
+?>
